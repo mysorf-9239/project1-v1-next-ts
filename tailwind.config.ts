0e6e797989914924164298d25e4565ec03eb2f19
mysorf-9239 <uydanh214@gmail.com>
2024-12-22 23:04:45 +0700

    fix: tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,20 @@
 import type { Config } from "tailwindcss";
+import colors from 'tailwindcss/colors';
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: colors.lime,
+        secondary: colors.stone,
       },
     },
   },
